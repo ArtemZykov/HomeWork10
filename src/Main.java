093d6199a4458f192185baf81a0ad1592664d10f
mac <artem.workamail.87@gmail.com>
2024-03-03 23:33:13 +0300

    HW#10 готовое

--- src/Main.java
+++ src/Main.java
@@ -4,33 +4,39 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main (String[] args) {
-        int year = 2021;
+        int year = 1600;
         checkLeapYear(year);
         int currentYear = LocalDate.now().getYear();
         int OS = 1;
         requiredDevice(OS, currentYear);
-        int deliveryDistance = 95;
+        int deliveryDistance = 101;
         requiredTime(deliveryDistance);
         System.out.println();
-
-
     }
         public static void checkLeapYear( int checkYear) {
-            if (checkYear >= 1584 && checkYear % 400 == 0 && checkYear % 100 != 0 || checkYear % 4 == 0) {
-                System.out.println(" "+ checkYear + " год является высокосным ");
-            } else {
-                System.out.println(" "+ checkYear + " год не является высокосным ");
+            if (checkYear < 1584) {
+                System.out.println(" до 1584 года, высокосные года не считали ");
             }
+            if (checkYear > 1584 && ( checkYear % 400 == 0 && checkYear % 100 != 0 || checkYear % 4 == 0)) {
+                    System.out.println(" " + checkYear + " год является высокосным ");
+            } else {
+                    System.out.println(" " + checkYear + "  год не является высокосным ");
+
+            }
+
+
+
+
             System.out.println();
         }
         public static void requiredDevice(int OS, int currenYear) {
             if (currenYear < 2015 && OS == 0) {
                 System.out.println(" Установите облегченную версию ПО для IOS  по ссылке ");
-            } else if (currenYear >= 2015 && OS == 0) {
+            } else if ( OS == 0) {
                 System.out.println(" Позже вам все равно необходимо будет установить облегченную версию для IOS");
             } else if (currenYear < 2015 && OS != 0) {
                 System.out.println(" Установи облегченную версию для Android по ссылке ");
-            } else if (currenYear >= 2015 && OS != 0) {
+            } else if (OS != 0) {
                 System.out.println(" Позже вам все равно необходимо будет установить облегченную версию для Android ");
             } else {
                 System.out.println(" неверно задана ПО ");
@@ -45,16 +51,11 @@ public class Main {
             if (deliveryDistance > 20 && deliveryDistance < 60) {
                 int days = 2;
                 System.out.println(" Доставка будет в течении " + days + " дней ");
-            }
-            if (deliveryDistance > 20 && deliveryDistance < 60) {
-                int days = 2;
-                System.out.println(" Доставка будет в течении " + days + " дней ");
-            }
-            if (deliveryDistance > 60 && deliveryDistance < 100) {
+
+            } else if (deliveryDistance <= 100) {
                 int days = 3;
                 System.out.println(" доставка будет в течении " + days + " дней");
-            }
-            if (deliveryDistance > 100) {
+            }if (deliveryDistance > 100) {
                 System.out.println(" Доставки нет ");
             }
         }
